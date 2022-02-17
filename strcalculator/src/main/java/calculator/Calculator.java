08ec22581dc5cd77eab841ef084f7e45f6206fe7
yesung <einzigerys@gmail.com>
2022-02-17 23:22:41 +0900

    진행중

--- strcalculator/src/main/java/calculator/Calculator.java
+++ strcalculator/src/main/java/calculator/Calculator.java
@@ -1,6 +1,27 @@
 package calculator;
 
-import java.util.function.IntBinaryOperator;
+import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntBinaryOperator;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 public class Calculator {
+    private int num = 0;
+
+    public int working(String nums){
+
+        List<String> numlist = Arrays.asList(nums.split(" "));
+
+        for (String num : numlist){
+
+        }
+
+
+
+        return num;
+    }
 }
--- strcalculator/src/test/java/calculator/CalculatorTest.java
+++ strcalculator/src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.function.IntBinaryOperator;
 import java.util.stream.Stream;
 
 import org.assertj.core.api.Assertions;
@@ -8,53 +9,64 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
-//    private Calculator calculator;
-//
-//    @BeforeEach
-//    void createCalculator(){
-//        this.calculator = new Calculator();
-//    }
+
+    @ParameterizedTest
+    @MethodSource("workingInputParam")
+    void doCalculator(String nums, int result){
+        Calculator calculator = new Calculator();
+        Assertions.assertThat(calculator.working(nums)).isEqualTo(result);
+    }
 
     @ParameterizedTest
     @MethodSource("calcInputParam")
-    void doCalculator(int num1, int num2, String sign, int result){
+    void workCalculatorisRight(int num1, int num2, String sign, int result){
         Assertions.assertThat(CalcOperator.workOperator(sign).operator(num1, num2)).isEqualTo(result);
     }
 
     @Test
-    void addCalculator() {
+    void addCalculatorisRight() {
         Assertions.assertThat(CalcOperator.ADD.operator(10, 20)).isEqualTo(30);
     }
 
     @Test
-    void subtractCalculator() {
+    void subtractCalculatorisRight() {
         Assertions.assertThat(CalcOperator.SUBTRACT.operator(20, 10)).isEqualTo(10);
     }
 
     @Test
-    void multiplyCalculator() {
+    void multiplyCalculatorisRight() {
         Assertions.assertThat(CalcOperator.MULTIPLY.operator(10, 10)).isEqualTo(100);
     }
 
     @Test
-    void divideZeroError(){
-        CalcOperator.DIVIDE.operator(0, 10);
-    }
-
-    @Test
-    void divideCalculator() {
+    void divideCalculatorisRight() {
         Assertions.assertThat(CalcOperator.DIVIDE.operator(10, 10)).isEqualTo(1);
     }
 
-    private static Stream<Arguments> calcInputParam() { // argument source method
+    @Test
+    void divideZeroError(){
+        Assertions.assertThatThrownBy(() -> {
+            CalcOperator.DIVIDE.operator(10, 0);
+        }).isInstanceOf(ArithmeticException.class);
+    }
+
+    private static Stream<Arguments> calcInputParam() {
         return Stream.of(
                 Arguments.of(10, 10, "+", 20),
                 Arguments.of(10, 10, "-", 0),
                 Arguments.of(10, 10, "*", 100),
                 Arguments.of(10, 10, "/", 1)
+        );
+    }
+
+    private static Stream<Arguments> workingInputParam() {
+        return Stream.of(
+                Arguments.of("10 + 10 - 20", 0)
         );
     }
 }
